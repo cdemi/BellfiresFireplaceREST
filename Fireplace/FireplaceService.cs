28a0c98735354877f2a0929e392f0c8b1e71360f
Christopher Demicoli <christopher.demicoli@outlook.com>
2020-01-25 10:58:04 +0100

    Added more resiliency

--- Fireplace/FireplaceService.cs
+++ Fireplace/FireplaceService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,33 +11,40 @@ namespace Fireplace
 {
     public class FireplaceService
     {
-        TcpClient client = new TcpClient();
+        TcpClient client;
         private NetworkStream networkStream;
         const string on = "0233303330333033303830314103";
         const string off = "0233303330333033303830313003";
         private readonly IPAddress fireplaceIP;
+        private readonly ILogger<FireplaceService> logger;
         byte[] onBytes = StringToByteArray(on);
         byte[] offBytes = StringToByteArray(off);
-        public FireplaceService(IConfiguration config)
+        public FireplaceService(IConfiguration config, ILogger<FireplaceService> logger)
         {
             fireplaceIP = IPAddress.Parse(config.GetValue<string>("FireplaceIP"));
             connect();
+            this.logger = logger;
         }
 
         private void connect()
         {
+            logger.LogInformation("Connecting to Fireplace");
+            client?.Close();
+            client = new TcpClient();
             client.Connect(fireplaceIP, 2000);
             networkStream = client.GetStream();
+            
         }
 
         public void TurnOn()
         {
+            logger.LogInformation("Turning Fireplace On");
             writeToSocket(onBytes);
-
         }
 
         public void TurnOff()
         {
+            logger.LogInformation("Turning Fireplace Off");
             writeToSocket(offBytes);
         }
 
@@ -45,7 +53,15 @@ namespace Fireplace
             if (!client.Connected)
                 connect();
 
-            networkStream.Write(data, 0, data.Length);
+            try
+            {
+                networkStream.Write(data, 0, data.Length);
+            }
+            catch (System.IO.IOException)
+            {
+                logger.LogWarning("Unable to communicate to Fireplace");
+                writeToSocket(data);
+            }
         }
 
         private static byte[] StringToByteArray(string hex)
